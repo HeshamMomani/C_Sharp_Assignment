282712adf023b3c5e23eb7fb343df045bad15496
Hesham Momani <hnalmomani19@cit.just.edu.jo>
2021-12-25 23:23:44 +0200

    First File

---
+++ catGirl/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace catGirl
+{
+    partial class Level1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainTimer1 = new System.Windows.Forms.Timer(this.components);
+            this.f2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.fruit = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.sc = new System.Windows.Forms.Label();
+            this.miss = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.f2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fruit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainTimer1
+            // 
+            this.MainTimer1.Enabled = true;
+            this.MainTimer1.Interval = 59;
+            this.MainTimer1.Tick += new System.EventHandler(this.GameTick);
+            // 
+            // f2
+            // 
+            this.f2.Image = global::catGirl.Properties.Resources.fruit;
+            this.f2.Location = new System.Drawing.Point(315, 66);
+            this.f2.Name = "f2";
+            this.f2.Size = new System.Drawing.Size(89, 104);
+            this.f2.TabIndex = 2;
+            this.f2.TabStop = false;
+            this.f2.Tag = "fruit";
+            // 
+            // player
+            // 
+            this.player.Image = global::catGirl.Properties.Resources.girl2;
+            this.player.Location = new System.Drawing.Point(402, 492);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(155, 161);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            this.player.Tag = "player";
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.LightCoral;
+            this.logo.Image = global::catGirl.Properties.Resources.logo2;
+            this.logo.Location = new System.Drawing.Point(315, 238);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(338, 214);
+            this.logo.TabIndex = 0;
+            this.logo.TabStop = false;
+            // 
+            // fruit
+            // 
+            this.fruit.Image = global::catGirl.Properties.Resources.fruit;
+            this.fruit.Location = new System.Drawing.Point(78, 84);
+            this.fruit.Name = "fruit";
+            this.fruit.Size = new System.Drawing.Size(89, 104);
+            this.fruit.TabIndex = 7;
+            this.fruit.TabStop = false;
+            this.fruit.Tag = "fruit";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::catGirl.Properties.Resources.fruit;
+            this.pictureBox5.Location = new System.Drawing.Point(765, 66);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(89, 104);
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "fruit";
+            // 
+            // sc
+            // 
+            this.sc.AutoSize = true;
+            this.sc.Font = new System.Drawing.Font("Freestyle Script", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sc.Location = new System.Drawing.Point(12, 0);
+            this.sc.Name = "sc";
+            this.sc.Size = new System.Drawing.Size(116, 42);
+            this.sc.TabIndex = 9;
+            this.sc.Text = "Score : 0";
+            // 
+            // miss
+            // 
+            this.miss.AutoSize = true;
+            this.miss.Font = new System.Drawing.Font("Freestyle Script", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.miss.Location = new System.Drawing.Point(747, 0);
+            this.miss.Name = "miss";
+            this.miss.Size = new System.Drawing.Size(127, 42);
+            this.miss.TabIndex = 10;
+            this.miss.Text = "Missed : 0";
+            // 
+            // Level1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(884, 665);
+            this.Controls.Add(this.miss);
+            this.Controls.Add(this.sc);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.fruit);
+            this.Controls.Add(this.f2);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.logo);
+            this.MaximizeBox = false;
+            this.Name = "Level1";
+            this.Text = "Level1";
+            this.Load += new System.EventHandler(this.Level1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.f2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fruit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox f2;
+        private System.Windows.Forms.Timer MainTimer1;
+        private System.Windows.Forms.PictureBox fruit;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label sc;
+        private System.Windows.Forms.Label miss;
+    }
+}
+
---
+++ catGirl/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace catGirl
+{
+    public partial class Level1 : Form
+    {
+        bool GoLeft, GoRight;
+        int Score = 0,  speed=12, Missed=0;
+
+        Random randA = new Random();
+        Random randB = new Random();
+        PictureBox drop = new PictureBox(); // create a new drop picture box, this will added dynamically
+
+        public Level1()
+        {
+            InitializeComponent();
+            Restart();
+        }
+
+        private void Level1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void GameTick(object sender, EventArgs e)
+        {
+           sc.Text = "Score :" + Score; //label
+         miss.Text = "Misssed :" + Missed;//label;
+           
+
+            // if the go left boolean is true AND player left is greater than 0
+            if (GoLeft == true && player.Left > 0)
+            {
+                // then we move the chicken 10 pixels to the left
+                player.Left -= 10;
+                //checken image will be change to the one moving left
+                player.Image = Properties.Resources.girl2;
+            }
+
+            //if the go right is true AND player width and left is less than form width
+            // meaning the player is still within the frame of the game
+            if (GoRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                // move the player 10 pixels to the right
+                player.Left += 10;
+                // change the player image to the one moving right
+                player.Image = Properties.Resources.girl2;
+            }
+
+            //below for loop will check the strawberry dropping from the top
+            // for each Control we are calling strawberry in this form
+            foreach (Control strawberry in this.Controls)
+            {
+               
+                // if strawberry is a type of picture box AND it has the tag of fruit
+                if (strawberry is PictureBox && (string)strawberry.Tag == "fruit")
+                {
+                    // then move strawberry towards the bottom
+                    strawberry.Top += speed;
+
+
+                    // if the strawberry reaches bottom of the screen
+                    if (strawberry.Top + strawberry.Height > this.ClientSize.Height)
+                    {
+                        // if the strawberry hit the floor then we show the drop image
+                        drop.Image = Properties.Resources.drop; // set the drop image
+                        drop.Location = strawberry.Location; // drop shows up on the same location as the strawberry
+                        drop.Height = 200; // set the height
+                        drop.Width = 150; // set the width
+                        drop.BackColor = System.Drawing.Color.Transparent; // apply transparent background to the picture box
+
+                        this.Controls.Add(drop); // add the drop picture to the form
+
+                        strawberry.Top = randA.Next(20, 100) * -1; // position the strawberry to a random  location
+                        strawberry.Left = randB.Next(5, this.ClientSize.Width - strawberry.Width); // position the strawberry to a random location
+                        Missed++; // add 1 to the missed integer
+
+                    }
+
+                    // if the strawberry bound with the player image
+                    // if both picture boxes collide
+                    if (strawberry.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        strawberry.Top = randA.Next(80, 100) * -1; // position the strawberry to a random  location
+                        strawberry.Left = randB.Next(5, this.ClientSize.Width - strawberry.Width); // position the strawberry to a random X location
+                        Score++; // add 1 to the score
+                    }
+
+                    // if the score is equals to or greater than 20
+                    if (Score >= 20)
+                    {
+                        speed = 16; // increase the strawberry speed to 20
+                    }
+
+                    // if the missed number is greater than 5
+                    // we need to stop the game
+                    if (Missed > 10)
+                    {
+                        MainTimer1.Stop(); // stop the game timer
+                        // show the message box to say game is over. 
+                        MessageBox.Show("Game Over!! We lost good Strawberry" + "\r\n" + "Click OK to Restart");
+                        // once the players clicks OK we restart the game again
+                        Restart();
+                    }
+
+                }
+
+            }
+
+        }
+
+      
+
+       
+     
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            //if left or rigth key pressed it going to change
+            if (e.KeyCode == Keys.Left)
+                GoLeft = true;
+            if (e.KeyCode == Keys.Right)
+                GoRight = true;
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            //keys are released
+            if (e.KeyCode == Keys.Left)
+                GoLeft = false;
+            if (e.KeyCode == Keys.Right)
+                GoRight = false;
+        }
+        public void Restart() { 
+        foreach(Control strawberry in this.Controls)
+            {
+                // friut move
+                if(strawberry is PictureBox && (string)strawberry.Tag=="fruit")
+                    // make fuits apper randomly
+                    strawberry.Top = randA.Next(5, 100) * -1; // -1 to move down
+                strawberry.Left = randB.Next(5, this.ClientSize.Width - strawberry.Width); // to apper random from left 5 and the right we should calculate it
+            }
+        // starting value
+            Score = 0;
+            speed = 5;
+            Missed = 0;
+
+            //start timer
+           MainTimer1.Start();
+
+           player.Left = this.ClientSize.Width / 2; // reset the player to middle of the form
+            player.Image = Properties.Resources.girl2; // change the player picture to face left
+
+
+
+        }
+    }
+}
